a3e1c837f939b9707e39e2e7071fd310861ec166
chaueq <chaueq@protonmail.com>
2020-01-18 02:05:38 +0100

    Switched errors and info from stdout to stderr

--- src/ui.c
+++ src/ui.c
@@ -3,31 +3,31 @@
 
 void printHelp(char* executable)
 {
-  printf("%s action input [output]\n", executable);
+  fprintf(stderr, "%s action input [output]\n", executable);
 
-  printf("\n\n%s\n\n", "action - one of following:");
-    printf("  %s  %s\n", "       help", "display this help");
-    printf("  %s  %s\n", "     --help", "same as 'help'");
-    printf("  %s  %s\n", "    encrypt", "encrypt text with Affine/Caesar cipher");
-    printf("  %s  %s\n", "         en", "same as 'encrypt");
-    printf("  %s  %s\n", "    decrypt", "decrypt text encrypted with Affine/Caesar cipher");
-    printf("  %s  %s\n", "         de", "same as 'decrypt'");
+  fprintf(stderr, "\n\n%s\n\n", "action - one of following:");
+    fprintf(stderr, "  %s  %s\n", "       help", "display this help");
+    fprintf(stderr, "  %s  %s\n", "     --help", "same as 'help'");
+    fprintf(stderr, "  %s  %s\n", "    encrypt", "encrypt text with Affine/Caesar cipher");
+    fprintf(stderr, "  %s  %s\n", "         en", "same as 'encrypt");
+    fprintf(stderr, "  %s  %s\n", "    decrypt", "decrypt text encrypted with Affine/Caesar cipher");
+    fprintf(stderr, "  %s  %s\n", "         de", "same as 'decrypt'");
 
-  printf("\n\n%s\n\n", "input - path to input file or:");
-    printf("  %s  %s\n", "      stdin", "read from stdin");
-    printf("  %s  %s\n", "          -", "same as 'stdin'");
-    printf("  %s  %s\n", "http[s]://…", "read from web resource");
+  fprintf(stderr, "\n\n%s\n\n", "input - path to input file or:");
+    fprintf(stderr, "  %s  %s\n", "      stdin", "read from stdin");
+    fprintf(stderr, "  %s  %s\n", "          -", "same as 'stdin'");
+    fprintf(stderr, "  %s  %s\n", "http[s]://…", "read from web resource");
 
-  printf("\n\n%s\n\n", "output - path to output file or:");
-    printf("  %s  %s\n", "     stdout", "print to stdout (default)");
-    printf("  %s  %s\n", "          -", "same as 'stdout'");
+  fprintf(stderr, "\n\n%s\n\n", "output - path to output file or:");
+    fprintf(stderr, "  %s  %s\n", "     stdout", "print to stdout (default)");
+    fprintf(stderr, "  %s  %s\n", "          -", "same as 'stdout'");
 
-    printf("\n");
+    fprintf(stderr, "\n");
 }
 
 void printError(char* errorText)
 {
-  printf("\n\033[1;31mERROR: %s\033[0m\n", errorText);
+  fprintf(stderr, "\n\033[1;31mERROR: %s\033[0m\n", errorText);
 }
 
 void printProgress(uintmax_t done, uintmax_t all)
@@ -36,26 +36,25 @@ void printProgress(uintmax_t done, uintmax_t all)
   const uintmax_t percentDone = (done*100) / all;
 
   if(percentDone < 100)
-    printf(" ");
+    fprintf(stderr, " ");
   if(percentDone < 10)
-    printf(" ");
-  printf("\033[1;37m%lu%% ", percentDone);
+    fprintf(stderr, " ");
+  fprintf(stderr, "\033[1;37m%lu%% ", percentDone);
 
-  printf("\033[0;42m"); //change background to green
+  fprintf(stderr, "\033[0;42m"); //change background to green
   for(uintmax_t i = 0; i < greenBlocks; ++i)
   {
-    printf(" ");
+    fprintf(stderr, " ");
   }
-  printf("\033[0;47m"); //change background to white
+  fprintf(stderr, "\033[0;47m"); //change background to white
   for(uintmax_t i = greenBlocks; i < PROGRESS_BAR_WIDTH; ++i)
   {
-    printf(" ");
+    fprintf(stderr, " ");
   }
-  printf("\033[0m"); //go back to default colors
+  fprintf(stderr, "\033[0m"); //go back to default colors
 
   if(percentDone == 100)
-    printf("\n");
+    fprintf(stderr, "\n");
   else
-    printf("\r");
-  fflush(stdout);
+    fprintf(stderr, "\r");
 }
